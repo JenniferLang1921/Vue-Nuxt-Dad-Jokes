5cd909f34459488cf657930e286c2e3f07f5022a
jenniferlang1921 <jenniferlang1921@gmail.com>
2021-02-14 19:21:17 -0600

    initial commit

---
+++ pages/about.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>About Dad Jokes</h1>
+    <p>This is an app that displays silly dad jokes</p>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "About Dad Jokes App",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Best place for silly dad jokes"
+        }
+      ]
+    }
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h2>Welcome to the best dad jokes app on the web</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "Welcome to Dad Jokes",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Best place for silly dad jokes"
+        }
+      ]
+    }
+  }
+};
+</script>
+
